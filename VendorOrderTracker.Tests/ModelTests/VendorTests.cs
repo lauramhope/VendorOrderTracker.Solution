1fdbdefaeac9ecfa793a97b71e2c18af9732fa6e
Laura Hope <ssteamlaura@gmail.com>
2023-05-12 08:29:26 -0700

    add passing test for creating Vendor constructor

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -5,5 +5,14 @@ using System;
 
 namespace VendorOrderTracker.Tests
 {
-  
+  [TestClass]
+  public class VendorTests
+  {
+    Vendor newVendor = new Vendor();
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
 }
