2c57a29283f774a1adb0559322845e7af40935e7
Laura Hope <ssteamlaura@gmail.com>
2023-05-12 09:27:01 -0700

    Add passing test for creating Order constructor

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -5,5 +5,15 @@ using System;
 
 namespace VendorOrderTracker.Tests
 {
-  
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+
 }
