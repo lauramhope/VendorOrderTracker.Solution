143b28123bb18fd4ff4887a10ba5b67ab7be6d14
Laura Hope <ssteamlaura@gmail.com>
2023-05-12 08:40:34 -0700

    Add passing test for creating auto-implemented Vendor Name property

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -8,11 +8,20 @@ namespace VendorOrderTracker.Tests
   [TestClass]
   public class VendorTests
   {
-    Vendor newVendor = new Vendor();
+    Vendor newVendor = new Vendor("Test Vendor");
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetVendorName_ReturnsVendorName_String()
+    {
+      string vendorName = "Test Vendor";
+      Vendor newVendor = new Vendor(vendorName);
+      string result = newVendor.VendorName;
+      Assert.AreEqual(vendorName, result);
     }
   }
 }
