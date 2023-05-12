ad2344bb0b5a17b460072bf7dfbc24f5a8f0c0c3
Laura Hope <ssteamlaura@gmail.com>
2023-05-12 10:25:39 -0700

    Add passing test for creating multiple orders, using GetAll() to return list of orders, and adding Order constructor _instnces for ID

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -2,4 +2,35 @@ using System.Collections.Generic;
 
 namespace VendorOrderTracker.Models
 {
+  public class Order
+  {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { };
+
+    public Order(string title, string description)
+    {
+      Title = title;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
+  }
 }
--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -2,5 +2,32 @@ using System.Collections.Generic;
 
 namespace VendorOrderTracker.Models
 {
-  
-}
+  public class Vendor
+  {
+    public string VendorName { get; set; }
+    public int Id { get; }
+    private static List<Vendor> _instances = new List<Vendor> { };
+
+    public Vendor(string vendorName)
+    {
+      VendorName = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
+  }
+} 
